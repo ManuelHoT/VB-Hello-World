c8501d7b74d929c881f991bd45d59d063fe2b873
manuel.gonzalez <manuel.gonzalez@houseoftest.se>
2022-03-21 12:08:24 +0100

    Add project files.

---
+++ Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("This is kinda cheating because the empty project defaults to Hello World.")
+        Console.WriteLine("I still hope I earn the team 5 points though.")
+    End Sub
+End Module
